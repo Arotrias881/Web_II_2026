23d30739dc89f801137d4f74cfc2566da11b9eb4
Arotrias881 <abigaelcarvajal321@gmail.com>
2026-02-24 10:14:44 -0400

    binario a decimal

---
+++ Unidad_1/Lab1/DecimalBinario.js
@@ -0,0 +1,3 @@
+const binario = "101010";
+const decimal = parseInt(binario, 2);
+console.log(decimal);
---
+++ Unidad_1/Lab1/DecimalBinarioFlecha.js
@@ -0,0 +1,2 @@
+const binarioADecimal = bin => parseInt(bin, 2);
+console.log(binarioADecimal("1010"))
